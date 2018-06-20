6dc1ed68f24761c7e40e9bab6387696cc46fb2fb
Branislav Lazic <branislav.lazic@codecentric.de>
2018-06-20 09:15:11 +0200

    Remove unnecessary breaks

--- vm/vm.go
+++ vm/vm.go
@@ -38,25 +38,20 @@ func (vm *VM) Run() {
 		case ICONST:
 			value := vm.fetch()
 			vm.stack.Push(value)
-			break
 		case IADD:
 			addResult := vm.stack.Pop() + vm.stack.Pop()
 			vm.stack.Push(addResult)
-			break
 		case ISUB:
 			subResult := vm.stack.Pop() - vm.stack.Pop()
 			vm.stack.Push(subResult)
-			break
 		case IMUL:
 			mulResult := vm.stack.Pop() * vm.stack.Pop()
 			vm.stack.Push(mulResult)
-			break
 		// "Jump" to instruction pointer unconditionally. E.g. JMP 2 sets the instruction
 		// pointer to 2 and starts executing program from that point.
 		case JMP:
 			jump := vm.fetch()
 			vm.instructionPointer = jump - 1
-			break
 		// "Jump" to instruction pointer conditionally. E.g. JMPT 2 will "pop" the value from
 		// stack and check if it's 1. If the condition is satisfied, then the instruction pointer
 		// will be set to 2. Otherwise, it will just continue with normal code execution.
@@ -65,56 +60,47 @@ func (vm *VM) Run() {
 			if vm.stack.Pop() == 1 {
 				vm.instructionPointer = jump - 1
 			}
-			break
 		case JMPF:
 			jump := vm.fetch()
 			if vm.stack.Pop() == 0 {
 				vm.instructionPointer = jump - 1
 			}
-			break
 		case ILT:
 			if vm.stack.Pop() < vm.stack.Pop() {
 				vm.stack.Push(1)
 			} else {
 				vm.stack.Push(0)
 			}
-			break
 		case IEQ:
 			if vm.stack.Pop() == vm.stack.Pop() {
 				vm.stack.Push(1)
 			} else {
 				vm.stack.Push(0)
 			}
-			break
 		// Makes a duplicate of stack top value and puts it
 		// back on the stack as a new top value
 		case COPY:
 			topValue := vm.stack.Peek()
 			vm.stack.Push(topValue)
-			break
 		case GLOAD:
 			address := vm.fetch()
 			globalValue := vm.globals[address]
 			vm.stack.Push(globalValue)
-			break
 		case GSTORE:
 			value := vm.stack.Pop()
 			address := vm.fetch()
 			vm.globals[address] = value
-			break
 		// Pop the value from the stack and store it in the local memory
 		// of the current frame
 		case STORE:
 			value := vm.stack.Pop()
 			address := vm.fetch()
 			vm.frameStack.Peek().variables[address] = value
-			break
 		// Load value from the local memory of the current frame
 		case LOAD:
 			address := vm.fetch()
 			frame := vm.frameStack.Peek()
 			vm.stack.Push(frame.variables[address])
-			break
 		// After CALL instruction has been processed, the next instruction is
 		// set as an address of the instruction pointer (to perform "jump"),
 		// then the instruction after that is being used to designate how many
@@ -129,18 +115,16 @@ func (vm *VM) Run() {
 			}
 			vm.frameStack.Push(frame)
 			vm.instructionPointer = jump - 1
-			break
 		case RET:
 			returnAddress := vm.frameStack.Peek().returnAddress
 			vm.frameStack.Pop()
 			vm.instructionPointer = returnAddress
 		case PRINT:
 			fmt.Println(vm.stack.Pop())
-			break
 		case HALT:
 			return
 		default:
-			break
+			return
 		}
 
 		if vm.flags["printStack"].(bool) {
