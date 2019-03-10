8075cb5264cc7f8648bcaea6e77bfdda5bbc0dc9
Branislav Lazic <brano2411@hotmail.com>
2019-03-10 17:51:51 +0100

    Add e2e test for global variables

---
+++ test/e2e/global_vars_test.go
@@ -0,0 +1,23 @@
+package e2e
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/BranislavLazic/rooster"
+)
+
+func TestGlobalVars(t *testing.T) {
+	content, err := rooster.LoadRCodeFile("./testdata/global_vars.rcode")
+	if err != nil {
+		t.Fatalf("cannot load an rcode file")
+	}
+
+	output := assertOutput(func(buf *bytes.Buffer) {
+		rooster.RunVM(content, nil, buf)
+	})
+	expectedRes := "42\n42\n3\n"
+	if output != expectedRes {
+		t.Fatalf("test - wrong output. expected=%s, got=%s", expectedRes, output)
+	}
+}
