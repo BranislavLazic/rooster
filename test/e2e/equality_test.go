5d939c7c0e9bfd82414df8804b8865abd199fe1b
Branislav Lazic <brano2411@hotmail.com>
2019-03-13 22:19:22 +0100

    Add e2e test for equality

---
+++ test/e2e/equality_test.go
@@ -0,0 +1,23 @@
+package e2e
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/BranislavLazic/rooster"
+)
+
+func TestEquality(t *testing.T) {
+	content, err := rooster.LoadRCodeFile("./testdata/equality.rcode")
+	if err != nil {
+		t.Fatalf("cannot load an rcode file")
+	}
+
+	output := assertOutput(func(buf *bytes.Buffer) {
+		rooster.RunVM(content, nil, buf)
+	})
+	expectedRes := "0\n1\n"
+	if output != expectedRes {
+		t.Fatalf("test - wrong output. expected=%s, got=%s", expectedRes, output)
+	}
+}
